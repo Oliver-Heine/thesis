b72854ccf2cad8202392675144e33749ab1b5fb4
MarcusTheCode <Marcusbyllemos@gmail.com>
2026-04-15 08:57:41 +0200

    Removed java backend

--- backend_server/backend/src/main/java/com/thesis/qrquishing/integrations/ai/AIAnalyzer.java
+++
@@ -1,8 +0,0 @@
-package com.thesis.qrquishing.integrations.ai;
-
-import com.thesis.qrquishing.UrlFeatures;
-import com.thesis.qrquishing.ValidationResponse;
-
-public interface AIAnalyzer {
-    public ValidationResponse analyze(String url, UrlFeatures features);
-}
